2ea74e8174edd75737af14b8805b8303b15bcc91
YoanGaussens <yoan.gaussens@gmail.com>
2025-03-11 20:02:12 +0100

    feat: :sparkles: Modification of the Descktop part - adding prestations - some little modif

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let phoneLink = document.querySelector(".phone-link");
-    let modal = document.getElementById("phone-modal");
-    let closeBtn = document.querySelector(".close");
+  let phoneLink = document.querySelector(".phone-link");
+  let modal = document.getElementById("phone-modal");
 
-    phoneLink.addEventListener("click", function (event) {
-        if (window.innerWidth >= 768) { // Seulement sur desktop
-            event.preventDefault(); // Empêche l'ouverture de l'app téléphone
-            modal.style.display = "flex";
-        }
-    });
+  phoneLink.addEventListener("click", function (event) {
+    if (window.innerWidth >= 768) {
+      // Seulement sur desktop
+      event.preventDefault(); // Empêche l'ouverture de l'app téléphone
+      modal.style.display = "flex";
+    }
+  });
 
-    // Fermer la modale en cliquant en dehors
-    window.addEventListener("click", function (event) {
-        if (event.target === modal) {
-            modal.style.display = "none";
-        }
-    });
+  // Fermer la modale en cliquant en dehors
+  window.addEventListener("click", closeModal);
+  window.addEventListener("touchstart", closeModal); // Pour les tablettes
+  function closeModal(event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  }
 });
